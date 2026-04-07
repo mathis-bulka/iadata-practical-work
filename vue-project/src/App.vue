801e953c939333bf04e37088ad2f418ea83b7bc5
BULKA Mathis <mathis.bulka@toulouse-metropole.fr>
2026-04-07 14:53:54 +0200

    Frontend updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <h1>Practical Work on Spotify API using VueJS and Spring Boot</h1>
+    <SearchBar />
   </main>
 </template>
 
---
+++ vue-project/src/components/SearchBar.vue
@@ -0,0 +1,3 @@
+<template>
+  <input></input>
+</template>
