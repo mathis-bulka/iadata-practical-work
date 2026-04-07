0fbd53cd1fa425e25a5afd6d4edda3f22c65b497
BULKA Mathis <mathis.bulka@toulouse-metropole.fr>
2026-04-07 14:49:30 +0200

    Initial commit

---
+++ practical-work-backend/src/main/java/fr/ToulouseMetropole/mathis/practical_work_backend/PracticalWorkBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.ToulouseMetropole.mathis.practical_work_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalWorkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalWorkBackendApplication.class, args);
+	}
+
+}
---
+++ practical-work-backend/src/test/java/fr/ToulouseMetropole/mathis/practical_work_backend/PracticalWorkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ToulouseMetropole.mathis.practical_work_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalWorkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
