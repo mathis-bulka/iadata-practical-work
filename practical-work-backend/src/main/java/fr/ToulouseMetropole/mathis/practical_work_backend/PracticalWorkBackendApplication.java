88c8f8eb8afdbf0a072ebdaf5b9326107df2a6a0
Mathis Bulka <sykman01@gmail.com>
2026-04-08 12:00:10 +0200

    artist id can now be passed as a parameter, even though i believe i did it the wrong way

--- practical-work-backend/src/main/java/fr/ToulouseMetropole/mathis/practical_work_backend/PracticalWorkBackendApplication.java
+++ practical-work-backend/src/main/java/fr/ToulouseMetropole/mathis/practical_work_backend/PracticalWorkBackendApplication.java
@@ -17,38 +17,44 @@ import fr.ToulouseMetropole.model.TokenResponse;
 public class PracticalWorkBackendApplication {
 
     private static final Logger log = LoggerFactory.getLogger(PracticalWorkBackendApplication.class);
+
     @Value("${spotify.client-id}")
     private String clientId;
+
     @Value("${spotify.client-secret}")
     private String clientSecret;
-    
+
     public static void main(String[] args) {
-        SpringApplication.run(PracticalWorkBackendApplication.class, args);
+        SpringApplication.run(PracticalWorkBackendApplication.class, "0TnOYISbd1XYRBk9myaseg");
     }
 
     @Bean
     @Profile("!test")
     public ApplicationRunner run(RestClient.Builder builder) {
-        RestClient restClient = builder.baseUrl("https://api.spotify.com").build();
-        System.out.println(clientId);
-        System.out.println(clientSecret);
+        RestClient restClient = builder.build();
 
         return args -> {
+            String artistId = args.getSourceArgs()[0];
             TokenResponse tokenResponse = restClient.post()
                     .uri("https://accounts.spotify.com/api/token")
                     .header("Content-Type", "application/x-www-form-urlencoded")
-                    .body("grant_type=client_credentials&client_id=" + clientId + "&client_secret="
-                            + clientSecret)
+                    .body("grant_type=client_credentials&client_id=" + clientId + "&client_secret=" + clientSecret)
                     .retrieve()
                     .body(TokenResponse.class);
 
+            if (tokenResponse == null || tokenResponse.access_token() == null) {
+                log.error("Failed to retrieve access token");
+                return;
+            }
+
             String token = tokenResponse.access_token();
 
-            ArtistAlbums albums = restClient
-                    .get().uri("/v1/artists/0TnOYISbd1XYRBk9myaseg/albums")
+            ArtistAlbums albums = restClient.get()
+                    .uri("https://api.spotify.com/v1/artists/{id}/albums", artistId)
                     .header("Authorization", "Bearer " + token)
                     .retrieve()
                     .body(ArtistAlbums.class);
+
             log.info(albums.toString());
         };
     }
